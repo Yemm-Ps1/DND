3c0f5332bedf2ff3a004d23554320e59fb641946
danielskyrme93 <danielskyrme@hotmail.com>
2021-07-05 17:54:46 +0100

    Parameter set of MakeAbilitySaveThrow adjusted to match begin-process pattern e.g. https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_parameter_sets?view=powershell-7.1#example. Function to be updated to allow for a raw dc check vs a RAW ABILITY MOD check to overide use of character class if object is non-existent.

--- src/AbilityUtil.Tests.ps1
+++ src/AbilityUtil.Tests.ps1
@@ -7,34 +7,50 @@ Import-Module .\src\RollUtil.ps1
 $CHARACTER_RONNIE = [player]::new("Ronnie McTest", "Ronnie", 10, 90, 1000, 10, 19, 15, 13, 12, 10, 8, 8, "Blue", {})
 # $CHARACTER_BOB = [player]::new("Bob McTest", "Bob", 7, 90, 1000, 10, 19, 15, 13, 12, 10, 8, 8, "Blue", {})
 
-$ALL_CASES = @(
-        @{AbilityType = [AbilityTypes]::Str; AdvModifier=-2}
-        @{AbilityType = [AbilityTypes]::Dex; AdvModifier=-1}
-        @{AbilityType = [AbilityTypes]::Con; AdvModifier=0}
-        @{AbilityType = [AbilityTypes]::Int; AdvModifier=1}
-        @{AbilityType = [AbilityTypes]::Wis; AdvModifier=2}
-        @{AbilityType = [AbilityTypes]::Cha; AdvModifier=0}
-    )
 $UNBEATABLE_DC = 1000
 $POWERLESS_DC = -1000
 
+# Describe "Tests Ability Saving Throws for DcVsMod parameter sets" {
+#     $ABILITY_SAVE_CASES = @(
+#             @{Ability_ModAdvModifier=-2}
+#             @{AdvModifier=-1}
+#             @{AdvModifier=0}
+#             @{AdvModifier=1}
+#             @{AdvModifier=2}
+#             @{AdvModifier=0}
+#         )
+# }
 
-Describe "Ability Saving Throw Tests " {
-    It "Tests MakeAbilitySaveThrow - against an UNBEATABLE DC with type <AbilityType> and roll modifier <AdvModifier> - should always FAIL" -TestCases $ALL_CASES {
-    param([AbilityTypes] $AbilityType, [int] $AdvModifier)
-    $test_result = MakeAbilitySaveThrow $AbilityType $UNBEATABLE_DC $CHARACTER_RONNIE $AdvModifier -Return
-    $test_result | Should -Be $false
+Describe "Tests Ability Saving Throws for DcVsCharacter parameter sets" {
+    $ABILITY_SAVE_CASES = @(
+            @{AbilityType = [AbilityTypes]::Str; AdvModifier=-2}
+            @{AbilityType = [AbilityTypes]::Dex; AdvModifier=-1}
+            @{AbilityType = [AbilityTypes]::Con; AdvModifier=0}
+            @{AbilityType = [AbilityTypes]::Int; AdvModifier=1}
+            @{AbilityType = [AbilityTypes]::Wis; AdvModifier=2}
+            @{AbilityType = [AbilityTypes]::Cha; AdvModifier=0}
+        )
+    
+    It "Tests MakeAbilitySaveThrow - with DcVsCharacter IGNORING ADVANTAGE MOD against an UNBEATABLE DC with type <AbilityType> and roll modifier <AdvModifier> - should always FAIL" -TestCases $ABILITY_SAVE_CASES {
+            param([AbilityTypes] $AbilityType, [int] $AdvModifier)
+            $test_result = MakeAbilitySaveThrow $AbilityType $UNBEATABLE_DC $CHARACTER_RONNIE -Return
+            $test_result | Should -Be $false
+    }
+    It "Tests MakeAbilitySaveThrow - with DcVsCharacter against an UNBEATABLE DC with type <AbilityType> and roll modifier <AdvModifier> - should always FAIL" -TestCases $ABILITY_SAVE_CASES {
+        param([AbilityTypes] $AbilityType, [int] $AdvModifier)
+        $test_result = MakeAbilitySaveThrow $AbilityType $UNBEATABLE_DC $CHARACTER_RONNIE -advantage_mod $AdvModifier -Return
+        $test_result | Should -Be $false
     }
 
-    It "Tests MakeAbilitySaveThrow - against an POWERLESS DC with type <AbilityType> and roll modifier <AdvModifier> - should always SUCCEED" -TestCases $ALL_CASES {
+    It "Tests MakeAbilitySaveThrow - with DcVsCharacter against an POWERLESS DC with type <AbilityType> and roll modifier <AdvModifier> - should always SUCCEED" -TestCases $ABILITY_SAVE_CASES {
         param([AbilityTypes] $AbilityType, [int] $AdvModifier)
-        $test_result = MakeAbilitySaveThrow $AbilityType $POWERLESS_DC $CHARACTER_RONNIE $AdvModifier -Return
+        $test_result = MakeAbilitySaveThrow $AbilityType $POWERLESS_DC $CHARACTER_RONNIE -advantage_mod $AdvModifier -Return
         $test_result | Should -Be $true
     }
 
     It "Tests MakeAbilitySaveThrow - with out of range advantage modifiers - should throw exception" {
-        {MakeAbilitySaveThrow ([AbilityTypes]::Str) 0 $CHARACTER_RONNIE -3 -Return} | Should -Throw
-        { MakeAbilitySaveThrow [AbilityTypes]::Str 0 $CHARACTER_RONNIE 3 -Return} | Should -Throw
+        {MakeAbilitySaveThrow ([AbilityTypes]::Str) 0 $CHARACTER_RONNIE -advantage_mod -3 -Return} | Should -Throw
+        { MakeAbilitySaveThrow [AbilityTypes]::Str 0 $CHARACTER_RONNIE -advantage_mod 3 -Return} | Should -Throw
         # $test_result | Should -Be $true
     }
     It "Tests GetProficiencyBonus - with level <Level> character - should have value <ExpectedProficiencyBonus>" -TestCases @(
@@ -52,5 +68,24 @@ Describe "Ability Saving Throw Tests " {
         $test_result = GetProficiencyBonus $level
         $test_result | Should -Be $ExpectedProficiencyBonus    
     }
+
+}
+
+Describe "Tests Skill Checks" {
+    Write-Host ">>>" "Doing skill checks"
+    It "Tests MakeSkillCheck - against an UNBEATABLE DC with mod <AbilityMod> on prof-type <ProfType> and advantage mod <AdvantageMod> - should always fail " -TestCases @(
+        @{DC = $UNBEATABLE_DC; AbilityMod = 0; ProfType = [ProficiencyTypes]::Normal; AdvantageMod = 0}
+    ) {
+            param([int] $DC, [int] $AbilityMod, [ProficiencyTypes] $ProfType, [int] $AdvantageMod)
+            Write-Host $DC $AbilityMod $ProfType $AdvantageMod
+            $test_result = MakeSkillCheck $DC $AbilityMod $ProfType $AdvantageMod Blue -return
+            $test_result | Should -Be $false
+    }
+
+}
+
+
+Describe "Tests Misc Functions" {
+    
 
 }
--- src/AbilityUtil.ps1
+++ src/AbilityUtil.ps1
@@ -30,6 +30,21 @@ enum SkillTypes {
     Survival							
 }
 
+enum ProficiencyTypes {
+    Normal
+    Proficient
+    Expertise
+}
+
+enum DifficultyClasses {
+    VeryEasy
+    Easy
+    Medium
+    Hard
+    VeryHard
+    NearImpossible
+}
+
 class SkilledEntity {
 
 }
@@ -49,71 +64,128 @@ class SkilledEntity {
 
 
 Function MakeAbilitySaveThrow {
+    # TODO Parameter Sets: DcVsMod, DcVsCharacter, CharacterVsMod, CharacterVsCharacter
+    # TODO replace player with character
+    [CmdletBinding(DefaultParameterSetName = 'DcVsMod')]
     Param(
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'DcVsCharacter',
+            HelpMessage = 'Enter a raw integer dc vs a character object',
+            Position = 0)]
         [AbilityTypes] $ability_type,
+
+        [Parameter(Mandatory = $true,
+            ParameterSetName = 'DcVsMod',
+            HelpMessage = 'Enter a raw integer dc vs a raw ability modifier',
+            Position = 0)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'DcVsCharacter', Position=1)]
         [int] $dc,
+        
+        [Parameter(ParameterSetName = 'DcVsMod')]
+        [int] $ability_mod = 0,
+        
+        [Parameter(Mandatory = $true, ParameterSetName = 'DcVsCharacter', Position=2)]
         [player] $defender,
+        
         [ValidateRange(-2, 2)] [int] $advantage_mod = 0, # -2=Super-Disadvantage -1=Disadvantage 0=Normal 1=Advantage 2=Super-Disadvantage
         [switch] $return
     )
+    begin {
+        if (!$dc) {
+            # TODO add character retrieval of DC
+        }
+        # Retrives the ability mod for a defending character, if the character was passed instead of a raw ability mod
+        if (!$ability_mod -and $defender) {
+            [stat]$stat = $null
+            switch ($ability_type){
+                Str {
+                    $stat = $defender.STR
+                }
+                Dex {
+                    $stat = $defender.DEX
+                }
+                Con {
+                    $stat = $defender.CON
+                }
+                Int {
+                    $stat = $defender.INT
+                }
+                Wis {
+                    $stat = $defender.WIS
+                }
+                Cha {
+                    $stat = $defender.CHA
+                }
+            }
+            $ability_mod = $stat.Mod
+            if ($stat.IsSavingProficient) {
+                $ability_mod += [math]::ceiling($defender.Level / 4) + 1
+            }
+        }
+    } process {
+    
+        # works out the type of throw(super-disadvantage=-2, ..., super-advantage=2) and whether to use max or min of dice (max if >0)
+        $saving_throw = RollMultiple ([math]::Abs($advantage_mod)+1) $ability_mod ($advantage_mod -ge 0)  -return 
+    
+        $was_passed = $saving_throw -ge $dc
+        WriteThrowResult $defender.colour $was_passed "saving throw" $dc
+    
+        if($return) {
+            Return $was_passed
+        }
 
-    [stat]$stat = $null
-    switch ($ability_type){
-        Str {
-            $stat = $defender.STR
-        }
-        Dex {
-            $stat = $defender.DEX
-        }
-        Con {
-            $stat = $defender.CON
-        }
-        Int {
-            $stat = $defender.INT
-        }
-        Wis {
-            $stat = $defender.WIS
-        }
-        Cha {
-            $stat = $defender.CHA
+    }
+}
+
+function MakeSkillCheck() {
+    # TODO add parameter sets for using characters instead of numbers
+    param(
+        [int] $dc,
+        [int] $ability_mod,
+        [ProficiencyTypes] $prof_type,
+        [ValidateRange(-2, 2)] [int] $advantage_mod = 0, # -2=Super-Disadvantage -1=Disadvantage 0=Normal 1=Advantage 2=Super-Disadvantage
+        [System.ConsoleColor] $thrower_color,
+        [switch] $return
+    )
+    $prof_mod = 0
+    switch($prof_type) {
+        Proficient {
+            $prof_mod = 5
+        } Expertise {
+            $prof_mod = 10
         }
     }
-    $ability_mod = $stat.Mod
-    if ($stat.IsSavingProficient) {
-        $ability_mod += [math]::ceiling($defender.Level / 4) + 1
-    }
-
-    # works out the type of throw(super-disadvantage=-2, ..., super-advantage=2) and whether to use max or min of dice (max if >0)
-    Write-Host ([int] $advantage_mod / 2) $ability_mod ($advantage_mod -ge 0)
-    $saving_throw = RollMultiple ([math]::Abs($advantage_mod)+1) $ability_mod ($advantage_mod -ge 0)  -return 
+    $add_modifier = $ability_mod + $prof_mod
+    $saving_throw = RollMultiple ([math]::Abs($advantage_mod)+1) $add_modifier ($advantage_mod -ge 0)  -return 
 
     $was_passed = $saving_throw -ge $dc
-    if ($was_passed) {
-        WriteSavingThrowResult $defender " passed their saving throw against " $dc
-    } else {
-        WriteSavingThrowResult $defender " failed their saving throw against " $dc
-    }
+    WriteThrowResult $thrower_color $was_passed "saving throw" $dc
 
     if($return) {
         Return $was_passed
     }
 }
 
+
 function GetProficiencyBonus {
     Param([int] $character_level)
     return [math]::ceiling($character_level / 4) + 1
 }
 
-function WriteSavingThrowResult {
-    param ([player] $defender, [string] $affix, [int] $dc)
-    Write-Host $defender.Name -ForegroundColor $defender.Colour -NoNewline
-    Write-Host $affix -NoNewline 
+function WriteThrowResult {
+    param ([System.ConsoleColor] $thrower_color, [bool] $was_success, [string] $throw_lbl, [int] $dc)
+    Write-Host $defender.Name -ForegroundColor Blue -NoNewline
+    if($was_success) {
+        Write-Host " saved their $throw_lbl against " -NoNewline 
+    } else {
+        Write-Host " failed their $throw_lbl against " -NoNewline 
+    }
     Write-Host $dc -ForegroundColor Yellow -NoNewline
     Write-Host .
 }
 
-# $test_character = [player]::new("Test Man", "Test Man", 10, 101, 0, 6, 16, 15, 14, 12, 11, 10, 8, "Blue")
-
-# $rtn_val = MakeAbilitySaveThrow ([AbilityTypes]::Str) 14 $test_character -2 -Return
+# $test_character = [player]::new("Test Man", "Test Man", 10, 101, 0, 6, 16, 15, 14, 12, 11, 10, 8, "Blue", {})
+# $test_character.STR.IsSavingProficient = $true
+# $rtn_val = MakeAbilitySaveThrow ([AbilityTypes]::Str) 14 $test_character -advantage_mod 1 -Return
 
 # Write-Host "Value found: " $rtn_val
